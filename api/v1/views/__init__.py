101803c7332a1fea61e5015ba93db69b76fa677c
root <root@DESKTOP-MOC38TV>
2024-01-07 00:37:50 +0100

    clean code

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Blueprint for the Flask web application API."""
+from .index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
-from .index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,4 +10,3 @@ from api.v1.views import app_views
 @app_views.route('/status', strict_slashes=False)
 def get_status():
     return jsonify({"status": "OK"})
-
