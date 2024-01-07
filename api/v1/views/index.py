ec40c22f3857fd6ab50c97eb0698c85dc4db616f
root <root@DESKTOP-MOC38TV>
2024-01-07 02:09:06 +0100

    add new tasks 4 but output it not organized

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,28 @@ object within the api.v1.views
 """
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def get_status():
     return jsonify({"status": "OK"})
+
+
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    """
+    Retrieves the number of each object by type.
+    """
+    try:
+        stats = {
+                "amenities": storage.count("Amenity"),
+                "cities": storage.count("City"),
+                "places": storage.count("Place"),
+                "reviews": storage.count("Review"),
+                "states": storage.count("State"),
+                "users": storage.count("User")
+                }
+        return jsonify(stats)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
