7cc2a716c9589c3826f280bbc3f1e7080c4c3d3e
root <root@DESKTOP-MOC38TV>
2024-01-07 02:19:41 +0100

    add new clean code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,10 +7,10 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False)
 def get_status():
     return jsonify({"status": "OK"})
-
 
 
 @app_views.route('/stats', strict_slashes=False)
