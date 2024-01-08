d75470385b027ca8a59481573391b65920441328
root <root@DESKTOP-MOC38TV>
2024-01-08 02:14:48 +0100

    masking passzrd

--- models/user.py
+++ models/user.py
@@ -13,11 +13,14 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column('password',String(128), 
+                nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship("Place", backref="user",
+                cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                cascade="all, delete-orphan")
     else:
         email = ""
         password = ""
@@ -27,3 +30,14 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """
+        masking password values
+        """
+        self._password = hashlib.md5(pwd.encode()).hexadigest()
