b02a59821204051bd66b7f8e65ba8de93cc498f2
root <root@DESKTOP-MOC38TV>
2024-01-08 13:30:15 +0100

    crypto password

--- models/user.py
+++ models/user.py
@@ -27,3 +27,18 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        if 'password' in kwargs:
+            self.update_password(kwargs['password'])
+
+    def update_password(self, new_password):
+        """
+        update the user password and hash it using md5
+        """
+        self.password = hashlib.md5(new_password.encode()).hexdigest()
+
+    def save(self):
+        """
+        save the current instance to the storage engine
+        """
+        self.update_password(self.password)
+        super().save()
